55e1f6c71b0559e92cc3e71b42472757d54b3535
Rashika Bhargava <rashika.bhargava@st.niituniversity.in>
2018-04-12 04:12:56 +0530

    Homepage complete

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+        Title = models.CharField(max_length=255,null=True)
+        pub_date = models.DateTimeField()
+        body = models.TextField()
+        image = models.ImageField(upload_to='images/')
+        
+    # body 
+    # image
+"""class Author(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
+
+class BlogTable(models.Model):
+    name = models.CharField(max_length=100)
+    tagline = models.TextField()
+
+class Entry(models.Model):
+    blog = models.ForeignKey(BlogTable, on_delete=models.CASCADE)
+    headline = models.CharField(max_length=255)
+    body_text = models.TextField()
+    pub_date = models.DateField()
+    mod_date = models.DateField()
+    authors = models.ManyToManyField(Author)
+    n_comments = models.IntegerField()
+    n_pingbacks = models.IntegerField()
+    rating = models.IntegerField()
+
+    def __str__(self):
+        return self.headline"""
+
+
